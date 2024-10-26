e0f76b8147223488aee33c30c8b2b08afa8b5e47
dfrojasley <dfrojas.feld@gmail.com>
2024-10-25 20:55:21 -0500

    Install Flow, first two functions converted to types

--- src/gallery_generator.js
+++ src/gallery_generator.js
@@ -1,5 +1,7 @@
+// @flow
 // this file is to add the html, css and base js files generation code
 // the functions in here are to be called from the terminal using the cli.js commander program
+
 const { htmlTemplate, cssTemplate, jsTemplate } = require('./data.js');
 const { copyImagesFromFolder, createThumbnails } = require('./image_utils.js');
 
@@ -7,30 +9,37 @@ const path = require('path');
 const fs = require('fs');
 
 /**
-* Creates an HTML file for the gallery.
-* @param {string} filePath - Path including the filename where the HTML file will be created.
-* @param {string} [title='My Gallery'] - The title to use in the HTML document (optional, defaults to 'My Gallery').
-* @param {string} [mainTitle='Gallery'] - The main title to display in the gallery (optional, defaults to 'Gallery').
-* @returns {Promise<void>} - A promise that resolves when the HTML file is successfully created or rejects if an error occurs.
-*/
-const generateHtmlFile = async (filePath, title = 'My Gallery', mainTitle = 'Gallery') => {
-    return new Promise((resolve, reject) => {
-        const dir = path.dirname(filePath);
-        // Ensure the directory exists
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-        }
-        const htmlContent = htmlTemplate(title, mainTitle);
-        // Write the HTML file
-        fs.writeFile(filePath, htmlContent, (err) => {
-            if (err) {
-                reject(err); // Reject the promise if there is an error
-                return;
-            }
-            console.log(`HTML file created at: ${filePath}`);
-            resolve(); // Resolve the promise after the file is successfully written
-        });
+ * Creates an HTML file for the gallery.
+ * @param {string} filePath - Path including the filename where the HTML file will be created.
+ * @param {string} [title='My Gallery'] - The title to use in the HTML document (optional, defaults to 'My Gallery').
+ * @param {string} [mainTitle='Gallery'] - The main title to display in the gallery (optional, defaults to 'Gallery').
+ * @returns {Promise<void>} - A promise that resolves when the HTML file is successfully created or rejects if an error occurs.
+ */
+const generateHtmlFile = async (
+  filePath: string,
+  title: string = 'My Gallery',
+  mainTitle: string = 'Gallery'
+): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const dir: String = path.dirname(filePath);
+    
+    // Ensure the directory exists
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+    const htmlContent: string = htmlTemplate(title, mainTitle);
+    
+    // Write the HTML file
+    fs.writeFile(filePath, htmlContent, (err: ?Error) => {
+      if (err) {
+        reject(err); // Reject the promise if there is an error
+        return;
+      }
+      console.log(`HTML file created at: ${filePath}`);
+      resolve(); // Resolve the promise after the file is successfully written
     });
+  });
 };
 
 
@@ -43,18 +52,21 @@ const generateHtmlFile = async (filePath, title = 'My Gallery', mainTitle = 'Gal
  * @param {number} [thumbSize=250] - The size of the thumbnails for the gallery (default is 250px)
  * @returns {Promise} - A promise that resolves when the CSS file is successfully created, or rejects if an error occurs
  */
-const generateCssFile = async (filePath, thumbSize = 250) => {
+const generateCssFile = async (
+    filePath: string,
+    thumbSize?: number = 250
+): Promise<void> => {
 
     return new Promise((resolve, reject) => {
 
-        const cssContent = cssTemplate(thumbSize);
+        const cssContent: string = cssTemplate(thumbSize);
         // Ensure the directory exists
-        const dir = path.dirname(filePath);
+        const dir: string = path.dirname(filePath);
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
         }
         // Write the CSS content to the specified file
-        fs.writeFile(filePath, cssContent, (err) => {
+        fs.writeFile(filePath, cssContent, (err: ?Error) => {
             if (err) {
                 reject(err); // Reject the promise if there's an error
                 return
